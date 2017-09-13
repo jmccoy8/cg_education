4d9ef7ca67cdc83e051f83ce0c2056d581abe70c
Jenessa McCoy <jenessa108@gmail.com>
2017-09-12 23:53:09 -0500

    did js assigment

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log("This file is linked!");
+var teacherName = "Professor Oak";
+var department = "Pokemon and Us";
+var rating1 = 4.3;
+var rating2 = 3.2;
+var rating3 = 4.9;
+console.log("Teacher: " + teacherName + "\n" + "Department: " + department + "\n" + rating1 + ", " + rating2 + ", " + rating3);
+var avgRating = (4.3+3.2+4.9) / 3;
+var avgRating= Math.round(10 *avgRating) / 10;
+console.log(avgRating);
+
+var studentName = "Ash";
+var major = "Pokemon Trainer";
+var buddy = "Pikachu";
+console.log("Student Name: " + studentName + "\n" + "Major: " + major + "\n" + "Buddy: " + buddy);
+
+var courseName = "Pokemon and Us";
+var courseTeacher = "Oak";
+var semester = "2017/2018";
+console.log("Course: " + courseName + "\n" + "Teacher: " + "\n" + semester);
