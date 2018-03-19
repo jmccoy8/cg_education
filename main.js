4fdcaa225f288e78fa60ab5ce375df1b97a75f6d
Jenessa McCoy <jenessa108@gmail.com>
2018-03-19 18:48:49 -0500

    added new trainer page and much more

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
 
-  var audio = $("audio#picsound")[0];
+  let audio = $("audio#picsound")[0];
   $(".pikpic").click(function() {
     audio.play();
   });
 
+  $("#char").click(function(){
+    alert("Congratulations! Take care of Charmander");
+  });
+
+  $("#squir").click(function(){
+    alert("Congratulations! Take care of Squirtle");
+  });
+
+  $("#bul").click(function(){
+    alert("Congratulations! Take care of Bulbasaur");
+  });
 });
